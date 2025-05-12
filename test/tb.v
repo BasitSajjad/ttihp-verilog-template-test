c0500c8c03f6a31cc6015e060df5f8a409bf7f50
BasitSajjad <113548884+BasitSajjad@users.noreply.github.com>
2025-05-12 18:27:17 +0500

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -3,10 +3,12 @@ module tb();
 // DUT signals
 reg clk;
 reg reset_n;
-reg [7:0] ui;
-reg [7:0] uio;
-wire [7:0] uo;
-
+reg ena;
+reg [7:0] ui_in;
+reg [7:0] uio_in;
+wire [7:0] uo_out;
+wire [7:0] uio_out;
+wire [7:0] uio_oe;
 
 // Test variables
 integer i, j;
@@ -18,8 +20,11 @@ integer errors;
 tt_um_sha256_shift_reg dut (
     .clk(clk),
     .reset_n(reset_n),
+    .ena(ena),
     .ui(ui),
-    .uio({6'b0, uio[1],uio[0]}),
+    .uio_out({6'b0, uio_out[1],1'b0}),
+    .uio_in({7'b0, uio_in[0]}),
+    .uio_oe({6'b0, 1'b1, 1'b0}),
     .uo(uo)
 );
 endmodule
